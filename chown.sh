78e4cdcdee3b1f2458a3fe19b25a49147e55892d
Ken Wronkiewicz <wh@wirewd.com>
2019-10-13 20:30:21 -0700

    add permission fixer.

---
+++ chown.sh
@@ -0,0 +1,1 @@
+docker run --env-file=local_settings -v `pwd`/smwconfig:/var/www/smwconfig -v `pwd`/conf:/var/www/conf -v `pwd`/data:/var/www/data -v `pwd`/localstore:/var/www/localstore wirehead/semantic-mediawiki-docker ensure-permissions.sh
